bc717f1d9eb69ac58e43461d7e3b1612fe63c70f
Juliano Targa <53620803+julianotarga@users.noreply.github.com>
2026-01-13 16:59:43 -0300

    fix: corrigir parent_uuid do menu Apps do FusionPBX
    - Alterado de 0438b504-... para fd29e39c-... (UUID correto do menu Apps)
    - Adicionado grupo 'user' no app_menu.php
    - Seguindo padrão dos apps oficiais (zoiper, sms)

--- fusionpbx-app/voice_secretary/app_menu.php
+++ fusionpbx-app/voice_secretary/app_menu.php
@@ -7,10 +7,11 @@ $y = 0;
 $apps[$x]['menu'][$y]['title']['en-us'] = "Voice Secretary";
 $apps[$x]['menu'][$y]['title']['pt-br'] = "Secretária Virtual";
 $apps[$x]['menu'][$y]['uuid'] = "b2c3d4e5-f6a7-8901-bcde-f12345678901";
-$apps[$x]['menu'][$y]['parent_uuid'] = "0438b504-8613-7887-c420-c837ffb20cb1";
+$apps[$x]['menu'][$y]['parent_uuid'] = "fd29e39c-c936-f5fc-8e2b-611681b266b5"; // Apps menu
 $apps[$x]['menu'][$y]['category'] = "internal";
 $apps[$x]['menu'][$y]['path'] = "/app/voice_secretary/secretary.php";
 $apps[$x]['menu'][$y]['groups'][] = "superadmin";
 $apps[$x]['menu'][$y]['groups'][] = "admin";
+$apps[$x]['menu'][$y]['groups'][] = "user";
 
 ?>
